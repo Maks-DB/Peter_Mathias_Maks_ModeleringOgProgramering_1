0ebc3ac8e52ede3f94e07ee200be32bbf16b9f57
silverzdk <naus.klint@gmail.com>
2023-03-01 11:15:26 +0100

    Alle terninge slag virker nu

--- drawOptionBox.m
+++ drawOptionBox.m
@@ -1,7 +1,13 @@
-function drawOptionBox(fig, rollOptions)
+%function drawOptionBox(fig, rollOptions)
 %------------------
+function drawOptionBox
+fig = uifigure;
 
-clc
+rollOptions = [1 2 3 4 5 6 4;
+              30 30 30 30 30 30 30;
+               1 1 1 1 1 1 1;
+               20 20 20 20 20 20 20];
+
 
 %------------------
 % Formattere array så den er nemmmere at arbejde med.
@@ -18,7 +24,7 @@ rollOptionsNoZero = [];
 %fjerner alle nul søjler
 for t = 1:numCol
     if rollOptions(1,t) ~= 0
-        % cat er concatenate, så vi tilføjer alle søjler der ikke 
+        % cat er concatenate, så vi tilføjer alle søjler der ikke
         % indeholder et 0, den tager 1 eller 2 som arugment alt efter
         % om den skal på under eller på siden.
         rollOptionsNoZero = cat(2,rollOptions(:,t),rollOptionsNoZero);
@@ -42,7 +48,7 @@ disp(rollOptions)
 %-----------------------------
 % bygger data array
 
-%vi laver et table og giver variablerne navne. 
+%vi laver et table og giver variablerne navne.
 dataArray = table('size',[numCol, 4],'VariableTypes',{'string','double','double','logical'});
 dataArray.Properties.VariableNames = ["Slag","Point","Terninger","Valg"];
 
@@ -52,29 +58,56 @@ for t = 1:numCol
     n = rollOptions(2,t);
 
     %checker type slag for hver søjle
+    switch n
 
-    %enkelt terning 
-    if n == 1
-        %Indsætter forklarende tekst
-        dataArray{t,1} = sprintf("Du har slået en %d'er ",rollOptions(1,t));
+        %enkelt terning
+        case 1
+            %Indsætter forklarende tekst
+            dataArray{t,1} = sprintf("Du har slået en %d'er ",rollOptions(1,t));
 
-        %indsætter antal point
-        dataArray{t,2} = rollOptions(4,t);
+            %indsætter antal point
+            dataArray{t,2} = rollOptions(4,t);
 
-        dataArray{t,3} = 1;
-    
-    %cameron
-    elseif n == 30
-        %Indsætter forklarende tekst
-        dataArray{t,1} = "Tilykke du har slået Cameron";
+            %en terning er en terning :)
+            dataArray{t,3} = 1;
 
-        %indsætter antal point
-        dataArray{t,2} = rollOptions(4,t);
+        %cameron
+        case 30
+            %Indsætter forklarende tekst
+            dataArray{t,1} = "Tilykke du har slået Cameron";
+
+            %indsætter antal point
+            dataArray{t,2} = rollOptions(4,t);
+
+            %cameron er altid 6 terninger
+            dataArray{t,3} = 6;
+            
+            %Undgå at tilføje alle cameron muligheder
+            dataArray(2:6,:) =[];
+            break
+
+        %3 par
+        case 40
+            %Indsætter forklarende tekst
+            dataArray{t,1} = "Tilykke du har slået 3 Par i første slag";
+
+            %indsætter antal point
+            dataArray{t,2} = rollOptions(4,t);
+
+            %3 par er altid 6 terninger
+            dataArray{t,3} = 6;
+
         
-        %cameron er altid 6 terninger 
-        dataArray{t,3} = 6;
+        otherwise
+            %Indsætter forklarende tekst
+            dataArray{t,1} = sprintf("Du har slået fødte %d'er",rollOptions(1,t));
 
-    elseif n == 
+            %indsætter antal point
+            dataArray{t,2} = rollOptions(4,t);
+
+            %Fødte er altdi 3 terninger
+            dataArray{t,3} = 3;
+
 
     end
 end
@@ -93,6 +126,8 @@ optionBox.Data = dataArray;
 
 %laver en knap til at vælge slaget
 continueButton = uicontrol(fig,'string', "Vælg Slag");
+continueButton.Position = [210 260 100 30];
+continueButton.BackgroundColor = 'g';
 %continueButton.Callback = 'uiresume(gcbf)'; - virker også
 %taget fra nettet varargin laver en function med variabel antal input
 %argumenter
